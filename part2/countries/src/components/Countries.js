31e47a4bb768533638ef7239a331d4c2781c2e33
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-23 22:19:02 +0200

    Solve 2.12

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -2,13 +2,33 @@ import { useState } from "react";
 
 const CountryName = ({ country: { name } }) => <p>{name}</p>;
 
+const LanguageList = ({ languages }) => (
+  <>
+    <h2>Languages</h2>
+    <ul>
+      {languages.map((l) => (
+        <li key={l.iso639_1}>{l.name}</li>
+      ))}
+    </ul>
+  </>
+);
+
+const CountryDetails = ({ country }) => (
+  <article>
+    <h1>{country.name}</h1>
+    <p>capital: {country.capital}</p>
+    <p>population: {country.population}</p>
+    <LanguageList languages={country.languages} />
+    <img src={country.flag} />
+  </article>
+);
+
 const Countries = ({ countries }) => {
   const [filter, setFilter] = useState("");
 
   const filtered = countries.filter((c) =>
     c.name.toLowerCase().includes(filter.toLowerCase())
   );
-  console.log({ filtered });
 
   return (
     <div>
@@ -18,8 +38,10 @@ const Countries = ({ countries }) => {
         <p>Too many matches, specify another filter</p>
       ) : filtered.length > 1 ? (
         filtered.map((c) => <CountryName key={c.numericCode} country={c} />)
+      ) : filtered.length == 1 ? (
+        <CountryDetails country={filtered[0]} />
       ) : (
-        <p></p>
+        <p>No country matches the filter</p>
       )}
     </div>
   );
