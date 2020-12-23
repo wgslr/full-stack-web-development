a47a31c1048bd972e2dfa78c3a8bed97e199ab4a
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-23 02:26:52 +0200

    Solve 1.1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,28 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) =>
+  parts.map(({ part, exercises }) => (
+    <p>
+      {part} {exercises}
+    </p>
+  ));
+
+const Total = ({ total }) => <p>Number of exercises {total}</p>;
+
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+      part: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      part: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      part: "State of a component",
+      exercises: 14,
+    },
+  ];
+  let total = 0;
+  parts.forEach(({ exercises }) => (total += exercises));
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
   );
 };
