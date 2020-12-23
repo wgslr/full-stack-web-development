417bed478526d9cfdb4dd54f0091e9c7dbca8b1e
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-23 21:18:02 +0200

    Solve 2.11: load initial persons from db

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Person = ({ name, number }) => (
   <p>
@@ -45,14 +46,14 @@ const Persons = ({ persons }) =>
   persons.map((p) => <Person {...p} key={p.name} />);
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3001/persons")
+      .then((resp) => setPersons(resp.data));
+  }, []);
 
   const handleAdd = (newPerson) => {
     if (persons.findIndex(({ name }) => name == newPerson.name) >= 0) {
