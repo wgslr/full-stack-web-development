bb6a02c6be4b92a58e7e6b9a6290a83d16dfcb33
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-23 03:05:45 +0200

    Solve 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood((v) => v + 1)}>good</button>
+      <button onClick={() => setNeutral((v) => v + 1)}>neutral</button>
+      <button onClick={() => setBad((v) => v + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
