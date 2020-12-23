7006c05dd61993e75927036b6d63d4f40f9f947b
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-23 22:59:30 +0200

    Solve 2.14: display weather data

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,5 +1,20 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
+const API_URL = "http://api.weatherapi.com/v1";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+const fetchWeather = async (city) => {
+  console.log("fetching weather");
+  const resp = await axios.get(API_URL + "/current.json", {
+    params: {
+      key: API_KEY,
+      q: city,
+    },
+  });
+
+  return resp.data.current;
+};
 const LanguageList = ({ languages }) => (
   <>
     <h2>Languages</h2>
@@ -11,6 +26,36 @@ const LanguageList = ({ languages }) => (
   </>
 );
 
+const Weather = ({ city }) => {
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  console.log("Weather component");
+  useEffect(() => {
+    fetchWeather(city)
+      .then((w) => {
+        setWeather({
+          icon: w.condition.icon,
+          text: w.condition.text,
+        });
+      })
+      .catch((err) => {
+        console.error("weather", err);
+        setError("Error fetching weather data");
+      });
+  }, []);
+
+  return weather ? (
+    <>
+      <h2>Weather in {city}</h2>
+      <p>{weather.text} </p>
+      <img src={weather.icon} />
+    </>
+  ) : error ? (
+    <p>{error}</p>
+  ) : null;
+};
+
 const CountryDetails = ({ country }) => (
   <article>
     <h1>{country.name}</h1>
@@ -18,6 +63,7 @@ const CountryDetails = ({ country }) => (
     <p>population: {country.population}</p>
     <LanguageList languages={country.languages} />
     <img src={country.flag} />
+    <Weather city={country.capital} />
   </article>
 );
 
@@ -28,9 +74,7 @@ const CountryInList = (props) => {
   } = props;
   const [showDetails, setShowDetails] = useState(false);
 
-  const toggle = () => {
-    setShowDetails((old) => !old);
-  };
+  const toggle = () => setShowDetails((old) => !old);
 
   return (
     <>
@@ -60,7 +104,7 @@ const Countries = ({ countries }) => {
         <p>Too many matches, specify another filter</p>
       ) : filtered.length > 1 ? (
         <ManyCountries countries={filtered} />
-      ) : filtered.length == 1 ? (
+      ) : filtered.length === 1 ? (
         <CountryDetails country={filtered[0]} />
       ) : (
         <p>No country matches the filter</p>
