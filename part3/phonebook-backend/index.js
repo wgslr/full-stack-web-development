c0462da69020330284148e11e4cac58ebf296fe3
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-28 00:29:18 +0200

    Add Procfile, read PORT from environment

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const MAX_ID = 1e9;
 
 morgan.token("body", (req, resp) =>
