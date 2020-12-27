245bf1ade55e4b85c73e302c8fc294dc1344129c
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-28 01:21:05 +0200

    Solve 3.12: add and list phonebook entries from CLI

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -13,11 +13,35 @@ const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
-
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person({ name, number });
-person.save().then(() => {
-  console.log(`added ${name} number ${number} to phonebook`);
+const printUsage = () => {
+  console.log(`Usage:
+mongo.js PASSWORD    list all persons
+mongo.js PASSWORD NAME NUMBER    add person NAME`);
   mongoose.connection.close();
-});
+};
+
+const addPerson = (name, number) => {
+  const newPerson = new Person({ name, number });
+  newPerson.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+};
+
+const printPeople = () => {
+  Person.find({}).then((persons) => {
+    console.log("phonebook:");
+    persons.forEach((p) => console.log(`${p.name} ${p.number}`));
+    mongoose.connection.close();
+  });
+};
+
+if (!name && !number) {
+  printPeople();
+} else if (!number || !name) {
+  printUsage();
+} else {
+  addPerson(name, number);
+}
