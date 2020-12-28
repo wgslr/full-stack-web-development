f8cbdb837d7c1e14f71b48b0ec7e2e6d67a0933b
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-12-28 02:45:11 +0200

    Validate field lengths

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -18,8 +18,8 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
 });
 personSchema.plugin(uniqueValidator);
 
