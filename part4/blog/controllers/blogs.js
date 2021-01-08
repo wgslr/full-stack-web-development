e43f7401dc154cef9adccc0911c5bb2418c169ab
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-08 20:02:42 +0200

    Solve 4.13: implement blog deletion

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,5 +1,7 @@
 const blogsRouter = require("express").Router();
+const { rawListeners } = require("../app");
 const Blog = require("../models/blog");
+const logger = require("../utils/logger");
 
 blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -21,6 +23,19 @@ blogsRouter.post("/", async (request, response) => {
 
   const saved = await blog.save();
   response.status(201).json(saved);
+});
+
+blogsRouter.delete("/:id", async (req, resp) => {
+  const id = req.params.id;
+
+  const result = await Blog.findByIdAndDelete(id);
+  if (result) {
+    logger.info(`Removed blog with id ${id}`);
+    resp.status(204).end();
+  } else {
+    logger.info("Blog to be removed not found");
+    resp.status(404).json({ error: "Blog to be removed does not exist" });
+  }
 });
 
 module.exports = blogsRouter;
