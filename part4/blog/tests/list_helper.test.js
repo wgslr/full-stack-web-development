77d9b2770bb360e5db68e332499690ce32f87c2c
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-08 15:16:01 +0200

    Solve 4.4: test "totalLikes" function

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -6,3 +6,62 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithThreeBlogs = [
+    {
+      _id: "6a422aa71b54a676234d17f8",
+      title: "Some blog",
+      author: "Blog author",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "7a422aa71b54a676234d17f8",
+      title: "Rome and Juliet",
+      author: "William Shakespeare",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 6,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 0,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("of bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithThreeBlogs);
+    expect(result).toBe(11);
+  });
+});
