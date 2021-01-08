82806f1ee9b97c715da6cbc1d26cfff3d5930bad
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-08 12:50:31 +0200

    Extract blogs controller to separate module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -4,15 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const { PORT, MONGODB_URI } = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const blogsRouter = require("./controllers/blogs");
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -23,20 +15,7 @@ mongoose.connect(MONGODB_URI, {
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
