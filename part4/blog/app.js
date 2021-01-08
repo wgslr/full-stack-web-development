3f9feb46af9b1bbd8c5c256ff1e6daa86b6ff8d5
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-08 17:12:28 +0200

    Separate app.js from index.js

---
+++ part4/blog/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { PORT, MONGODB_URI } = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,23 +1,10 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const { PORT, MONGODB_URI } = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
+const app = require("./app"); // the actual Express app
+const http = require("http");
+const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
