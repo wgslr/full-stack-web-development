410e1100dc5f326628b191a709fd2b3c7cf576e3
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-10 19:12:33 +0200

    Solve 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "./Blog";
 
 test("hides extended info", () => {
@@ -19,4 +19,24 @@ test("hides extended info", () => {
   expect(component.container).toHaveTextContent("Some title");
   expect(component.container).not.toHaveTextContent("/some/url");
   expect(component.container).not.toHaveTextContent("3 likes");
+});
+
+test("shows extended info", () => {
+  const blog = {
+    title: "Some title",
+    author: "The Author",
+    user: {
+      id: "123",
+    },
+    likes: 3,
+    url: "/some/url",
+  };
+
+  const component = render(<Blog blog={blog} />);
+  const showButton = component.getByText("View");
+  fireEvent.click(showButton);
+
+  expect(component.container).toHaveTextContent("Some title");
+  expect(component.container).toHaveTextContent("/some/url");
+  expect(component.container).toHaveTextContent("3 likes");
 });
