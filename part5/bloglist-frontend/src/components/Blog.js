5cf0d0ddccb9b2d9f606100481fd0050489cbd95
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-10 15:55:53 +0200

    Solve 5.7: add togglable blog details view

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,21 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React from "react";
+import Togglable from "./Togglable";
 
-export default Blog
+const Blog = ({ blog }) => (
+  <div
+    style={{
+      border: "solid 1px black",
+      margin: "0.3em 0",
+    }}
+  >
+    {blog.title} {blog.author}
+    <Togglable name="View">
+      <p>{blog.url}</p>
+      <p>
+        {blog.likes} <button>Like</button>
+      </p>
+    </Togglable>
+  </div>
+);
+
+export default Blog;
