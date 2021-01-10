3d17506acbbb81e28f44ece26a793bcbdb565745
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-10 12:56:42 +0200

    Solve 5.1: implement login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import axios from "axios";
+
+const logIn = async (username, password) => {
+  // will throw on non-200 error code
+  const resp = await axios.post("http://127.0.0.1:3001/api/login", {
+    username,
+    password,
+  });
+  const { name, token } = resp.data;
+  return { username, name, token };
+};
 
 const LoginForm = ({ onLoggedIn }) => {
+  const [error, setError] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log({ username, password });
+    logIn(username, password)
+      .then((userData) => {
+        onLoggedIn(userData);
+      })
+      .catch((e) => setError(e.message));
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
+        {error && <p>Error: {error}</p>}
         <p>
           <label>
             username{" "}
@@ -45,18 +62,27 @@ const LoginForm = ({ onLoggedIn }) => {
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  const onLoggedIn = (userData) => setUser(userData);
+
   return (
     <div>
       <h2>blogs</h2>
-      <LoginForm />
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {user ? (
+        <>
+          <p>Logged in as {user.name}</p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
+      ) : (
+        <LoginForm onLoggedIn={onLoggedIn} />
+      )}
     </div>
   );
 };
