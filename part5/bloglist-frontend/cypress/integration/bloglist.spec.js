a6e5b9467883bb2e88cc850544ba2006ecf142d1
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-11 15:21:47 +0200

    Solve 5.20: test blog like

--- part5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -31,15 +31,28 @@ describe("Blog app", function () {
     });
   });
 
-  describe.only("When logged in", function () {
+  describe("When logged in", function () {
     beforeEach(function () {
       cy.request("POST", "http://localhost:3001/api/login", {
         username: "tester",
         password: "password",
       }).then((response) => {
-        localStorage.setItem("user", JSON.stringify(response.body));
-        cy.visit("http://localhost:3000");
+        const user = response.body;
+        localStorage.setItem("user", JSON.stringify(user));
+        cy.request({
+          method: "POST",
+          url: "http://localhost:3001/api/blogs",
+          body: {
+            title: "Old Blog",
+            author: "Old Author",
+            likes: 3,
+            url: "/old/url",
+          },
+          auth: { bearer: user.token },
+        });
       });
+
+      cy.visit("http://localhost:3000");
     });
 
     it("A blog can be created", function () {
@@ -49,6 +62,13 @@ describe("Blog app", function () {
       cy.get("#urlInput").type("/new/url");
       cy.contains("Create").click();
       cy.get("#blog-list").contains("new title new author");
+    });
+
+    it("A blog can be liked", function () {
+      cy.contains("View").click();
+      cy.contains("3 likes");
+      cy.contains("Like").click();
+      cy.contains("4 likes");
     });
   });
 });
