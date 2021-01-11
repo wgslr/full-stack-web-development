8632a60eb4f101a535a1d20d9436ae1981d48511
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-11 19:13:30 +0200

    Solve 6.5: sort anecdotes

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -16,15 +16,17 @@ const App = () => {
     <div>
       <h2>Anecdotes</h2>
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+      {[...anecdotes]
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <AnecdoteForm />
     </div>
