77e0349658e5765200a01d7c68a50cabf57d6519
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-11 20:13:35 +0200

    Solve 6.13: load data from backend

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
+import { initAction } from "./reducers/anecdoteReducer";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    (async () => {
+      const resp = await axios.get("/anecdotes");
+      dispatch(initAction(resp.data));
+    })().catch((e) => console.error(e));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,4 @@
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
+const anecdotesAtStart = [];
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -36,6 +29,8 @@ const reducer = (state = initialState, action) => {
       );
     case "ADD":
       return state.concat(asObject(action.data.content));
+    case "INIT":
+      return action.data.anecdotes;
     default:
       return state;
   }
@@ -49,6 +44,11 @@ export const voteAction = (anecdoteId) => ({
 export const addAnecdoteAction = (data) => ({
   type: "ADD",
   data,
+});
+
+export const initAction = (anecdotes) => ({
+  type: "INIT",
+  data: { anecdotes },
 });
 
 export default reducer;
