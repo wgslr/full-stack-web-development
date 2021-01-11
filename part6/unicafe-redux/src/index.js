6818936b450c46e4a3d412641470e2bdb1eaffc0
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-01-11 17:49:07 +0200

    Solve 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
+  const increment = (kind) => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
+      type: kind,
+    });
+  };
+
+  const reset = () => {
+    store.dispatch({ type: "ZERO" });
+  };
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => increment("GOOD")}>good</button>
+      <button onClick={() => increment("OK")}>neutral</button>
+      <button onClick={() => increment("BAD")}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
